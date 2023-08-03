a751e7b71ff6623cf79916d13d9ceb6d09dd512d
oxahex <oxahex@gmail.com>
2023-08-03 18:12:10 +0900

    leetcode: prefix sum 문제 기록

---
+++ src/leetcode/LEETCODE_42.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * @date    2023-08-03
+ * @title   Trapping Rain Water(42), Hard
+ * @tags    Array, Two Pointers, DP(Dynamic Programming), Stack, Monotonic Stack
+ * @input
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ * @output
+ * compute how much water it can trap after raining.
+ */
+public class LEETCODE_42 {
+
+}
